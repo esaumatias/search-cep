558648254976c26c5934168ac09f26c9ac47f9f8
esaumatias <esaufreirematias@gmail.com>
2022-09-16 18:49:48 -0300

    feat(Home) - cria dropDown com seleçao de formato de listagem

--- src/Screens/index.js
+++ src/Screens/index.js
@@ -1,14 +1,35 @@
-import React, { useState } from 'react';
-import { StyleSheet, TextInput, View, TouchableOpacity, Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, TextInput, View, TouchableOpacity, Text, Alert } from 'react-native';
+import DropDownPicker from 'react-native-dropdown-picker';
+import { Button } from "@react-native-material/core";
+import { fetchCep } from '../Services/index';
 
 export default function Home() {
   const [cep, setCep] = useState("");
+  const [listCeps, setListCeps] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState(null);
+  const [items, setItems] = useState([
+    {label: 'UF - Cidade', value: 'UF - Cidade'},
+    {label: 'Cidade - Logradouro', value: 'Cidade - Logradouro'}
+  ]);
 
   const maskCep = (cep) => {
+    setCep(cep);
     cep = cep.replace(/\D/g, '')
     .replace(/(\d{5})(\d)/, '$1-$2')
     .replace(/(-\d{3})\d+?$/, '$1')
     setCep(cep);
+  };
+
+  const setCepList = async () => {
+    const response = await fetchCep(cep.replace(/\D/g, ""));
+    if (response.erro) {
+      Alert.alert("Cep Inválido");
+    } else {
+      setListCeps([...listCeps, response]);
+    }
+    setCep("");
   };
 
   return (
@@ -23,12 +44,25 @@ export default function Home() {
         />
       </View>
       <View style={styles.containerButton}>
-        <TouchableOpacity
+        <Button
           style={styles.buttonAdd}
+          onPress={setCepList}
           disabled={cep.length !== 9}
-        >
-          <Text style={styles.textAdd}>ADICIONAR</Text>
-        </TouchableOpacity>
+          title="Adicionar"
+          variant="outlined"
+          color="#202020"
+        />
+      </View>
+      <View>
+        <DropDownPicker
+          style={styles.dropDown}
+          open={open}
+          value={value}
+          items={items}
+          setOpen={setOpen}
+          setValue={setValue}
+          setItems={setItems}
+        />
       </View>
     </View>
   );
@@ -64,11 +98,11 @@ const styles = StyleSheet.create({
     width: "40%",
     marginTop: 10,
   },
-  textAdd: {
-    color: "#202020",
-    fontSize: 14,
-    fontWeight: "bold",
-    alignSelf: "center",
-    padding: 10,
+  dropDown: {
+    backgroundColor: "#f7dd52",
+    borderRadius: 10,
+    width: "44%",
+    marginTop: 10,
+    alignSelf: "flex-end"
   },
 });
